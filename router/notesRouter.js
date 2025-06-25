6415ae09c2e653865f7b7cff19190fb5029ed816
Mohd Rihan <137299852+Mohd-Rihan-101@users.noreply.github.com>
2025-06-26 02:41:43 +0530

    add router

--- router/notesRouter.js
+++ router/notesRouter.js
@@ -4,11 +4,14 @@ const router = express.Router();
 const {createNote} = require('../controllers/noteController');
 const {getNote} = require('../controllers/noteController');
 const {updateNote} = require('../controllers/noteController');
+const {deleteNote} = require('../controllers/noteController');
+
 
 
 
 router.post('/', createNote);
 router.get('/', getNote);
 router.put('/:id', updateNote);
+router.delete('/:id', deleteNote);
 
 module.exports = router;
