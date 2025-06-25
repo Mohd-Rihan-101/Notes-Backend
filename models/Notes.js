93777a28074f716da56b9b805791c0ba4248d000
Mohd Rihan <137299852+Mohd-Rihan-101@users.noreply.github.com>
2025-06-25 20:38:00 +0530

    add model

---
+++ models/Notes.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const notesSchema = mongoose.Schema({
+    title : {
+        type : String,
+        required : true
+    },
+    content : {
+        type : String,
+        required : true
+    }
+});
+
+const Notes = mongoose.model('Notes', notesSchema);
+module.exports = Notes;
