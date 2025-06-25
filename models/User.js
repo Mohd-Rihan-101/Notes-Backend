b1367c44e7a99bca810d05bf2acfbd6fbdc11dc9
Mohd Rihan <137299852+Mohd-Rihan-101@users.noreply.github.com>
2025-06-25 20:23:14 +0530

    add authmiddleware

--- models/User.js
+++ models/User.js
@@ -1,16 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-    username : {
-        type : String,
-        required : true
-    },
-    password : {
-        type : String,
-        required : true,
-        unique : true
-    }
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    unique: true,
+  },
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 module.exports = User;
